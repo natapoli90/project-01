1b8441890a77bd4550e6f76a22dbdec171bc059b
Natalia Polishchuk <Natalia@Natalias-MacBook-Pro.local>
2016-07-26 12:01:38 -0700

    change code to share

--- codeToShare.js
+++
@@ -1,8 +0,0 @@
-// calculate activity time
-$.get('/api/activities').success(function (activities) {
-  activities.forEach(function calculateTime (activity, met) {
-    var weightKg = (weight / 2.205);
-    var metKg = (activity.met * weightKg);
-    activity.time = Math.round(((calories / metKg)/0.01));
-  });
-  });
